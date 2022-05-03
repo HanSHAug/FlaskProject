510a49d2bad2fa2cd189ca66c9934d30109fbfa4
han <2222@ddd.c>
2022-05-03 19:14:14 +0900

    addbotton

--- __init__.py
+++ __init__.py
@@ -1,19 +1,95 @@
 from datetime import datetime
-from flask import Flask
+from flask import Flask, flash, session, redirect
 from flask import request
-from flask import render_template
+from flask import render_template, url_for
+from flask import flash
+from flask_sqlalchemy import SQLAlchemy
 import json
 import pymysql 
 
 class err(Exception):
     def __init__(self):
-        print("석일뱃살")
+        print("무게입력X")
 
 app = Flask(__name__)
 
+# app.secret_key = 'sqasd12asdzxcsada'
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if session.get('id'):
+        return render_template('inp.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        request_id = request.form['id']
+        request_password = request.form['password']
+        if len(request_id)==0 & len(request_password)==0:
+            return 'id or password not found'
+        else:
+            db = pymysql.connect(host='ec2-3-101-124-183.us-west-1.compute.amazonaws.com', \
+            port=3306, user='root', passwd='1234', db='health', charset='utf8') 
+            cursor = db.cursor(pymysql.cursors.DictCursor)
+            sql = """
+            SELECT * FROM user where id='%s'; 
+            """ % request_id
+            cursor.execute(sql)
+            result = cursor.fetchall() #result = [{'id': 'abcd', 'password': 1234}]
+            try:   
+                result = result[0]
+                # print(result)        # result = {'id': 'abcd', 'password': 1234}
+                # print(request_id)   # abcd
+                # print(request_password) # 1234
+                if result['id'] == request_id and result['password'] == int(request_password):
+                    session['id'] = request_id
+                    return render_template('inp.html')
+                else:
+                    return render_template('index.html')
+            except:
+                flash("잘못된 정보입니다.")
+                return redirect('/')
+            
+
+    else:
+        return '잘못된 접근'
+
+@app.route('/logout', methods=['POST','GET'])
+def logout():
+    session.clear()
+    return redirect('/')
+
+    # request_id = request.form.get('id')
+    # try:
+    #     if request_id == session['id']:
+    #         return 'already login'
+    #     else:
+    #         return 'error'
+    # except:
+    #     request_password = request.form.get('password')
+    #     db = pymysql.connect(host='ec2-3-101-124-183.us-west-1.compute.amazonaws.com', \
+    #         port=3306, user='root', passwd='1234', db='health', charset='utf8') 
+    #     cursor = db.cursor()
+    #     sql = """
+    #     SELECT * FROM user where id='%s'; 
+    #     """%request_id
+        
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     if result[0][1] == int(request_password):
+    #         session['id'] = request_id
+    #     print(result)
+    #     db.commit()
+    #     db.close()
+
+    #     return 'a'
+
+@app.route('/inp')
+def inp():
+    return render_template('inp.html')
 
 @app.route('/view')
 def view():
@@ -53,15 +129,15 @@ def input():
         port=3306, user='root', passwd='1234', db='health', charset='utf8') 
     cursor = db.cursor()
     sql = """
-    INSERT INTO datas VALUES('%s',%s,'%s');
-    """ % (now1, weight, imagePATH+finalFileName)
+    INSERT INTO datas VALUES('%s',%s,'%s','%s');
+    """ % (now1, weight, imagePATH+finalFileName, session['id'])
     cursor.execute(sql)
     db.commit()
     db.close()
     #INSERT INTO datas VALUES('1999-08-22 11:11:11',83.55,'/home/ec2-user/FlaskProject/images/20220423_105411.jpg')
 
-
-    return 'hello'
+    flash('업로드 완료')
+    return render_template('inp.html')
 
 @app.route('/body', methods =['GET'])
 def body():
@@ -69,8 +145,8 @@ def body():
         port=3306, user='root', passwd='1234', db='health', charset='utf8') 
     cursor = db.cursor()
     sql = """
-    SELECT * FROM datas; 
-    """
+    SELECT * FROM datas where user_id ='%s'; 
+    """ % session['id']
     cursor.execute(sql)
     
     result = cursor.fetchall()
@@ -100,4 +176,6 @@ def body():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True, host="0.0.0.0")
